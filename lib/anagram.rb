d092b9c7b003c1a450b4fb5e64102da2148bc379
Rexman17 <a.stone1794@gmail.com>
2018-10-10 18:08:34 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,31 @@
 # Your code goes here!
+require 'pry'
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word) # takes a word on initialization
+    @word = word
+  end
+
+  
+    def match(arr_of_words)
+      new_arr = []
+      arr_of_words.each do |string|
+        if string.size == @word.size
+          str1 = string.split("").sort
+          str2 = @word.split("").sort
+          if str1 == str2
+            new_arr << string
+          end
+        end
+      end
+    new_arr
+
+    end
+
+end
+
+
+# word
+# => ["hello", "world", "zombies", "pants", "dipper"]
