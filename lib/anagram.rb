9f7011649252681291687b5af0e171571e508442
Eric Laitman <ericlaitman@Erics-MacBook-Pro-2.local>
2018-10-13 20:48:22 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
-# Your code goes here!
+require 'pry'
+class Anagram
+
+
+  def initialize(word)
+    @word = word
+
+  end
+
+  def match(array)
+    array.select{|match| match.split("").sort.join("") == @word.split("").sort.join("")}
+  end
+
+end
